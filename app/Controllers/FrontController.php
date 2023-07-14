3ca613681cedc5970875dbfb27b92b7d0da4ed0e
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-07-14 16:46:43 +0530

    Update FrontController.php

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -215,8 +215,7 @@ class FrontController extends BaseController
     */
     public function add_collection_partial_cron()
     {
-        echo "test work";
-        die();   
+        echo "test work";  die();     
         $get_lates_colection = $this->user_model->get_cron_collection(); //get recently updated collection
         if (!empty($get_lates_colection)) { //check data empty or not  
 
