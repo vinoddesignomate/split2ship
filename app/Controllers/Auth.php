e2159a62f8c952a20740a1533df90eb1ee94f5ef
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-07-27 18:50:17 +0530

    Update Auth.php

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -179,9 +179,9 @@ class Auth extends BaseController
 			);
 
 			$userModel->insert_webhooks($track_webhook);
-			$plan_details = $userModel->get_store_plan($_GET['shop']);
-			echo"<pre>"; print_r($plan_details); echo"</pre>";
-			die();
+			$plan_details = $userModel->get_store_plane($_GET['shop']);
+			// echo"<pre>"; print_r($plan_details); echo"</pre>";
+			// die();
 			if (empty($plan_details)){
 				$plane_start_date = date('Y-m-d');
 				$plane_start_endate = date('Y-m-d', strtotime('+30 days'));
