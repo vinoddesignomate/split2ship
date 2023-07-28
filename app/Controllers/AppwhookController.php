7c829f5d3fcbb51ded5a5912299610613cde1b70
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-07-28 16:55:59 +0530

    Update AppwhookController.php

--- app/Controllers/AppwhookController.php
+++ app/Controllers/AppwhookController.php
@@ -50,10 +50,11 @@ class AppwhookController extends BaseController
 
      
 
-        $get_charge_id = $this->user_model->get_charge_id($_GET['shop']);
+        $get_charge_id = $this->user_model->get_charge_id($shop_header);
         //cancel charge when uninstalled app     
+        $get_resulsts = $this->user_model->get_tokens($shop_header);
         if (isset($get_charge_id[0]->charged_id) && $get_charge_id[0]->charged_id != "") {
-            $cancel_charge_id = $this->common->rest_api('/admin/api/2023-04/recurring_application_charges/' . $get_charge_id[0]->charged_id . '.json', array(), 'DELETE', $get_details->access_token, $_GET['shop']);
+            $cancel_charge_id = $this->common->rest_api('/admin/api/2023-04/recurring_application_charges/' . $get_charge_id[0]->charged_id . '.json', array(), 'DELETE', $get_resulsts->access_token, $shop_header);
             $cancel_charge_id_res = json_decode($cancel_charge_id['body'], true);
         }
 
