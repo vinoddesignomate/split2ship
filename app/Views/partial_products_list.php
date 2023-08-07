47b105b41c78e531ad39f81ca928053a14715bf1
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-08-07 23:53:05 +0530

    Update partial_products_list.php

--- app/Views/partial_products_list.php
+++ app/Views/partial_products_list.php
@@ -63,7 +63,7 @@ $store_name = $shop_name[0];
                                                     <input type="hidden" name="proid" value="<?php echo esc($list_product->product_id); ?>">
                                                     <input type="hidden" name="update_id" value="<?php echo esc($list_product->id); ?>">
                                                     <input type="button" name="cancel_per" class="payxnowandrestondelivery-cancel_btn" canid="<?php echo esc($list_product->id); ?>" value="cancel">
-                                                    <input type="button" class="partial_update_price" subid="<?php echo esc($list_product->id); ?>" name="update_per" value="Save">
+                                                    <input type="button" class="partial_update_price payxnowandrestondelivery-subbtn" subid="<?php echo esc($list_product->id); ?>" name="update_per" value="Save">
                                                 </span>
                                             </form>
                                         </td>
