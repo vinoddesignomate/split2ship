c048319e7d896807a47a25ed6297b828d8b61338
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-08-10 23:31:12 +0530

    Update app_configuration.php

--- app/Views/app_configuration.php
+++ app/Views/app_configuration.php
@@ -191,11 +191,11 @@ $store_name = $shop_name[0];
                                 </div>
                                 <div class="flex-row">
                                         <label for="">Full payment color code :</label>
-                                        <input type="text" name="fullbtn_color" value="<?php echo (isset($gtbtncolor[0]->full_btn_color) ? $gtbtncolor[0]->full_btn_color : '2F3030'); ?>">
+                                        <input type="text" data-jscolor="{}" name="fullbtn_color" value="<?php echo (isset($gtbtncolor[0]->full_btn_color) ? $gtbtncolor[0]->full_btn_color : '2F3030'); ?>">
                                 </div>
                                 <div class="flex-row">
                                         <label for="">Checkout button text color code :</label>
-                                        <input type="text" name="chk_btn_color" value="<?php echo (isset($gtbtncolor[0]->chk_btn_color) ? $gtbtncolor[0]->chk_btn_color : 'ffffff'); ?>">
+                                        <input type="text" data-jscolor="{}" name="chk_btn_color" value="<?php echo (isset($gtbtncolor[0]->chk_btn_color) ? $gtbtncolor[0]->chk_btn_color : 'ffffff'); ?>">
                                 </div>
                                 <div class="btn-row">
                                         <button type="submit" name="track_color" class="payxnowandrestondelivery-button payxnowandrestondelivery-main-cta" value="submit">Submit</button>
