e7d7752a6896d55d30ff01c0f6f360315fda41ec
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-08-22 20:16:39 +0530

    update produts
    update produts

--- app/Views/partial_products_list.php
+++ app/Views/partial_products_list.php
@@ -104,9 +104,9 @@ $store_name = $shop_name[0];
                             if ($part_page > 1) {
                                 echo "<li><a onclick='abc(event)' href='https://admin.shopify.com/store/" . esc($store_namep) . "/apps/pay-x-now-rest-on-delivery/partial-products-list?part_page=" . ($part_page - 1) . "' class='payxnowandrestondelivery-button'>Previous</a></li>";
                             }
-                            for ($i = 1; $i <= $total_pages; $i++) {
-                                echo "<li><a onclick='abc(event)' href='https://admin.shopify.com/store/" . esc($store_namep) . "/apps/pay-x-now-rest-on-delivery/partial-products-list?part_page=" . $i . "'>" . $i . "</a></li>";
-                            };
+                            // for ($i = 1; $i <= $total_pages; $i++) {
+                            //     echo "<li><a onclick='abc(event)' href='https://admin.shopify.com/store/" . esc($store_namep) . "/apps/pay-x-now-rest-on-delivery/partial-products-list?part_page=" . $i . "'>" . $i . "</a></li>";
+                            // };
                             if ($total_pages > $part_page) {
                                 echo "<li><a onclick='abc(event)' href='https://admin.shopify.com/store/" . esc($store_namep) . "/apps/pay-x-now-rest-on-delivery/partial-products-list?part_page=" . ($part_page + 1) . "' class='payxnowandrestondelivery-button'>Next</a></li>";
                             }
