5cd2021166f61e3a7827b4059e60fb6b24ab00dc
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-08-28 17:26:56 +0530

    js code
    js code

--- app/Views/partial_products_list.php
+++ app/Views/partial_products_list.php
@@ -199,7 +199,7 @@ $store_name = $shop_name[0];
         $("body").on("click", "#lstsearchq", function() {
             var getsearchval = $("#list_search").val();
             if (getsearchval.length >= 3) {
-                var currentURL = window.location.href;
+                var currentURL = top.window.location;
                 alert(currentURL);
                 var newParameter = 'newParam=newValue';
                 if (currentURL.indexOf('?') !== -1) {
