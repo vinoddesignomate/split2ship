dc308ae21f1965bb88aa56a9d0fa488597600715
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-09-12 18:59:10 +0530

    open in new window
    open in new window

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -25,7 +25,12 @@ $fstore_name = $shop_name[0];
         // return false;
 
     }
-
+    function new_window_opensplit2ship(event) {
+        event.preventDefault();
+        var href = event.currentTarget.getAttribute('href')
+        //top.window.location = href;
+        top.window.open(href, '_blank');
+    }
     function abc(event) {
         event.preventDefault();
         var href = event.currentTarget.getAttribute('href')
