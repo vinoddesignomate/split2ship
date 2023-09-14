b69e9d6490d522057cecf36c85f657e79a988a51
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-09-15 00:19:09 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -89,14 +89,14 @@ class Home extends BaseController
             } else {
                 $get_updated_plan = $this->user_model->get_store_plane($_GET['shop']);
 
-                if ($_GET['shop'] == 'desinomatetest.myshopify.com') {
+               //if ($_GET['shop'] == 'desinomatetest.myshopify.com') { 
 
-                    $get_register_webhook = $this->common->rest_api('/admin/api/2023-04/recurring_application_charges/30371381562.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
-                    $get_register_webhookset = json_decode($get_register_webhook['body'], true);
+                    // $get_register_webhook = $this->common->rest_api('/admin/api/2023-04/recurring_application_charges/30371381562.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
+                    // $get_register_webhookset = json_decode($get_register_webhook['body'], true);
 
-                    echo "get_register_webhookset<pre>"; print_r($get_register_webhookset); echo"</pre>";
+                    // echo "get_register_webhookset<pre>"; print_r($get_register_webhookset); echo"</pre>";
                 //     // $this->common->rest_api('/admin/api/2022-07/webhooks.json', array("webhook" => array("topic" => "orders/paid", "address" => 'https://app.payxnowandrestondelivery.com/markpaidorderemail?split_mark_paid_shop='.$_GET['shop'], "format" => "json")), 'POST', $get_details->access_token, $_GET['shop']);
-                 }
+                // }
 
                 if ($this->request->getPost('assign_save')) {
                     // print_r($this->request->getPost());
