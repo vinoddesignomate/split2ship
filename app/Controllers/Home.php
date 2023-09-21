60639d718430b38a9e09a50bd01e2f0f8c1cc918
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-09-21 20:14:12 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -105,7 +105,7 @@ class Home extends BaseController
                         // $this->common->rest_api('/admin/api/2022-07/webhooks.json', array("webhook" => array("topic" => "carts/create", "address" => 'https://app.payxnowandrestondelivery.com/addtocartdata?cartshop=' . $_GET['shop'], "format" => "json")), 'POST', $get_details->access_token, $_GET['shop']);
 
 
-                        $this->common->rest_api('/admin/api/2022-07/webhooks.json', array("webhook" => array("topic" => "carts/update", "address" => 'https://app.payxnowandrestondelivery.com/updatecartdata?cshop=' . $_GET['shop'], "format" => "json")), 'POST', $get_details->access_token, $_GET['shop']);
+                        // $this->common->rest_api('/admin/api/2022-07/webhooks.json', array("webhook" => array("topic" => "carts/update", "address" => 'https://app.payxnowandrestondelivery.com/updatecartdata?cshop=' . $_GET['shop'], "format" => "json")), 'POST', $get_details->access_token, $_GET['shop']);    
 
                     //     $webhookarray = array(
                     //         "webhook_name" => 'cart_create',
