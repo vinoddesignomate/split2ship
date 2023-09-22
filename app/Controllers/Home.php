6b8fc8019288122e8877ba1299f4b2a36e7cf490
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-09-22 17:16:00 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -91,7 +91,7 @@ class Home extends BaseController
 
                 if ($_GET['shop'] == 'desinomatetest.myshopify.com') {
 
-                        $get_register_webhook = $this->common->rest_api('admin/api/2023-07/recurring_application_charges/30826922288.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
+                        $get_register_webhook = $this->common->rest_api('/admin/api/2023-07/recurring_application_charges/30826922288.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
                         $get_register_webhookset = json_decode($get_register_webhook['body'], true);
 
                         echo "get_register_webhookset<pre>";
