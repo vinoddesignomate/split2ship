d8b14e3efe164014394f0c3174376a42ab71a6ce
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-09-28 19:51:02 +0530

    export all data
    export all data

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2198,6 +2198,7 @@ class Home extends BaseController
             }
         }
         $data['gtbtncolor'] = $this->user_model->get_checkout_button_color($_GET['shop']);
+        $data['get_allzip'] = $this->user_model->get_all_zipcodes($_GET['shop']);
         echo view('templates/header');
         echo view('app_configuration', $data);
         echo view('templates/footer');
