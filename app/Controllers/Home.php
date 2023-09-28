c1b564e0ff0542368219164a912d8a06ba11227d
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-09-28 16:41:35 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2156,11 +2156,15 @@ class Home extends BaseController
             echo "<script>top.window.location='https://admin.shopify.com/store/" . $this->shope_name . "/apps/pay-x-now-rest-on-delivery/app-configuration'</script>";
         }
         if ($_GET['shop'] == 'desinomatetest.myshopify.com') {
-        //if ($this->request->getPost('upload_zip')) {
-            echo"<pre>";print_r($this->request->getPost()); echo "</pre>";
-            echo"<pre>"; print_r($_FILES); echo "</pre>";
-            //die();
-       // }
+            if ($this->request->getPost('upload_zip')) {
+                echo "<pre>";
+                print_r($this->request->getPost());
+                echo "</pre>";
+                echo "<pre>";
+                print_r($_FILES);
+                echo "</pre>";
+                //die();
+            }
         }
         $data['gtbtncolor'] = $this->user_model->get_checkout_button_color($_GET['shop']);
         echo view('templates/header');
