d056f8d7fbf77d690d3cde6b09f1f4bbee565144
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-03 20:06:35 +0530

    Update welcome_message.php

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -314,7 +314,7 @@
             <div class="payxnowandrestondelivery-container">
                 <div class="payxnowandrestondelivery-main-area payxnowandrestondelivery-no-sidebar payxnowandrestondelivery-single-page">
                     <div class="payxnowandrestondelivery-head-wrapper">
-                        <h2 class="">Upload Zip Codes</h2>
+                        <h2 class="">Patial payment by Postal codes</h2>
                         <p><input type="checkbox" name="zip_en_dis" <?php if ($get_details->zip_code_enable_disabled == 1) { ?> checked <?php } ?> style="float: left;width: 1%;">Enable Partial payment by postal code</p>
                     </div>
                     <?php if ($get_details->zip_code_enable_disabled == 0) { ?>
