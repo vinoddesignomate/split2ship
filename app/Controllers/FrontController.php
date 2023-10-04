3faf015a7c673bcf9ed60df6960e0e378933e273
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-04 15:34:07 +0530

    Update FrontController.php

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -832,7 +832,7 @@ class FrontController extends BaseController
         $zipode = $body_data_decode['zipode'];
         $get_allzip = $this->user_model->get_all_zipcodes($shopname, $zipode);
         //echo"<pre>"; print_r($get_allzip); echo "</pre>";
-        $zip_regex = "/^[1-9]{6}(?:-[1-9]{4})?$/"; 
+        $zip_regex = "/^[1-9]{1}[0-9]{2}\\s{0,1}[0-9]{3}$/"; 
         $pregmatchvar = preg_match($zip_regex, $zipode);
         echo $pregmatchvar;
         echo "invalid";
