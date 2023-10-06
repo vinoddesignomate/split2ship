7d9554e208778cb60b1c2dd04edf77594b393d05
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-06 19:54:58 +0530

    Update FrontController.php

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -256,12 +256,12 @@ class FrontController extends BaseController
         $chekpartial = 0;
         $remaining_price = 0;
         $illp = 0;
-        // if ($_SERVER['HTTP_X_FORWARDED_FOR'] == '103.80.119.106') {
-        //     echo "<pre>";
-        //     print_r($cartarray);
-        //     echo "</pre>";
-        //     die();
-        // }
+        if ($_SERVER['HTTP_X_FORWARDED_FOR'] == '103.80.119.106') {
+            echo "<pre>";
+            print_r($cartarray);
+            echo "</pre>";
+            die();
+        }
         $ilosku = 1;
         foreach ($cartarray as $item_cart) {
 
