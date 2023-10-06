cc62cbd8fd19b4951d52bdfb3160e898eda6e622
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-06 17:20:53 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2140,6 +2140,7 @@ class Home extends BaseController
         } else {
             $update_order_count = $this->plane_details[$_REQUEST['planname']]['partial_product'];
         }
+        print_r($plan_details);
         if ($plan_details[0]->plan_name == $_REQUEST['planname'] && $plan_details[0]->plan_validity == $get_status['recurring_application_charge']['billing_on']) {
             $update_data = array(
                 "shop_url" => $_GET['shop'],
