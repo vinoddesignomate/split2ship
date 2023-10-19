e921564160676edc56c9919f15215be7ee4b33b8
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-19 18:15:19 +0530

    Update exchange_return.php

--- app/Views/exchange_return.php
+++ app/Views/exchange_return.php
@@ -1,31 +1,33 @@
 <style>
     .formContainer {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 1.25rem;
-}
-.headerForm {
-    font-style: normal;
-    font-weight: 600;
-    font-size: 20px;
-    line-height: 28px;
-    color: #202223;
-    width: 100%;
-    text-overflow: ellipsis;
-    overflow: hidden;
-    text-align: center;
-}
-.Polaris-Labelled__LabelWrapper {
-    word-wrap: break-word;
-    word-break: break-word;
-    overflow-wrap: break-word;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: baseline;
-    margin-bottom: var(--p-space-1);
-}
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 1.25rem;
+    }
+
+    .headerForm {
+        font-style: normal;
+        font-weight: 600;
+        font-size: 20px;
+        line-height: 28px;
+        color: #202223;
+        width: 100%;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        text-align: center;
+    }
+
+    .Polaris-Labelled__LabelWrapper {
+        word-wrap: break-word;
+        word-break: break-word;
+        overflow-wrap: break-word;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+        align-items: baseline;
+        margin-bottom: var(--p-space-1);
+    }
 </style>
 <div class="formContainer customInput" style="--bg-color: #000000; --opacity: 0.50; --radius: 24px;">
     <div class="headerForm">Return Center</div>
@@ -56,6 +58,27 @@
                 </div>
             </div>
         </div>
-    </div><button class="_xButton_l3r25_2 _fromLogin_l3r25_22 _block_l3r25_38" disabled="" style="cursor: default;"><span class="_content_l3r25_56"><span>Start Your Return</span></span></button><span style="text-align: center; word-break: break-word;"><span class="Polaris-TextStyle--variationSubdued">You have a maximum of 30 days from the date of your purchase to make a return.</span></span>
+    </div><button id="split_exchng" class="_xButton_l3r25_2 _fromLogin_l3r25_22 _block_l3r25_38" disabled="" style="cursor: default;"><span class="_content_l3r25_56"><span>Start Your Return</span></span></button><span style="text-align: center; word-break: break-word;"><span class="Polaris-TextStyle--variationSubdued">You have a maximum of 30 days from the date of your purchase to make a return.</span></span>
     <div class="line"></div><span class="cursor-pointer leading-[24px]">View the full return policy</span>
 </div>
+<script type="text/javascript">
+    const orderNumberInput = document.getElementById('order_number_val');
+    const orderEmailInput = document.getElementById('order_email_val');
+    const submitButton = document.getElementById('submit_button');
+
+    // Add event listeners to the text input fields
+    orderNumberInput.addEventListener('input', checkInputs);
+    orderEmailInput.addEventListener('input', checkInputs);
+
+    // Function to check both input fields and enable the button if both have content
+    function checkInputs() {
+        const orderNumberValue = orderNumberInput.value.trim();
+        const orderEmailValue = orderEmailInput.value.trim();
+
+        if (orderNumberValue !== '' && orderEmailValue !== '') {
+            submitButton.removeAttribute('disabled');
+        } else {
+            submitButton.setAttribute('disabled', 'disabled');
+        }
+    }
+</script>
