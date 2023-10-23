222928ace462ddea5d2bc3317c23dee4df776d26
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-23 21:54:11 +0530

    Update ExchangeappModel.php

--- app/Models/ExchangeappModel.php
+++ app/Models/ExchangeappModel.php
@@ -18,7 +18,7 @@ class ExchangeappModel extends Model
     public function track_exchange_orders($data_array, $shopurl)
     {
 
-        $qbuilder_insert = $this->db->table('orders');
+        $qbuilder_insert = $this->db->table('cg_exchange_return');
         $qbuilder_insert->where('order_id', $data_array['order_id']);
         $qbuilder_insert->where('shop_url', $shopurl);
         $qgetordpro = $qbuilder_insert->get();
@@ -29,12 +29,12 @@ class ExchangeappModel extends Model
         if (!empty($qgetordpro->getResult())) {
 
             //$all_result = $q->getResult();
-            $this->db->table('orders')->where('order_id', $data_array['order_id'])->update($data_array);
+            $this->db->table('cg_exchange_return')->where('order_id', $data_array['order_id'])->update($data_array);
             //return $all_result[0]->id;
             return '2';
         } else {
 
-            $this->db->table('orders')->insert($data_array);
+            $this->db->table('cg_exchange_return')->insert($data_array);
             // return $this->db->insertID();
             return '1';
         }
@@ -42,16 +42,16 @@ class ExchangeappModel extends Model
     public function track_orders_products_exchange($data_array)
     {
 
-        $qbuilder = $this->db->table('orders_products');
+        $qbuilder = $this->db->table('cg_exchange_return_order_products');
         $qbuilder->where('product_id', $data_array['product_id']);
         $qbuilder->where('order_id', $data_array['order_id']);
         $qbuilder->where('shop_url', $data_array['shop_url']);
         $q = $qbuilder->get();
         // $qbuilder->countAllResults();
         if (empty($q->getResult())) {
-            return  $this->db->table('orders_products')->insert($data_array);
+            return  $this->db->table('cg_exchange_return_order_products')->insert($data_array);
         } else {
-            $this->db->table('orders_products')->where('order_id', $data_array['order_id'])->where('product_id', $data_array['product_id'])->where('shop_url', $data_array['shop_url'])->update($data_array);
+            $this->db->table('cg_exchange_return_order_products')->where('order_id', $data_array['order_id'])->where('product_id', $data_array['product_id'])->where('shop_url', $data_array['shop_url'])->update($data_array);
             return '';
         }
     }
