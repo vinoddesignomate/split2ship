f12cb557da968b809f11ce07da970a3da4709b56
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-23 23:43:42 +0530

    Update exchange_return.php

--- app/Views/exchange_return.php
+++ app/Views/exchange_return.php
@@ -62,7 +62,7 @@
                     })
                     .then(response => response.json())
                     .then(response => {
-
+                        console.log(response);
 
                     })
                     .catch(error => {
