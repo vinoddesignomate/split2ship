275b363682e95a78118331264f4763f8d92fd368
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-24 23:56:47 +0530

    css changes for config videos
    css changes for config videos

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -1013,26 +1013,7 @@ class FrontController extends BaseController
             $get_details = $this->user_model->get_tokens($body_data_decode['shopname']);
 
 
-            /* $getvarimg = $this->common->rest_api('/admin/api/2023-07/variants/44320678019376.json', array(), 'GET', $get_details->access_token, $body_data_decode['shopname']);
-
-            $getvrntimg = json_decode($getvarimg['body'], true);
-            //echo"<pre>"; print_r($getvrntimg); echo "</pre>";
-
-            
-            $getimfsrcdata = $this->common->rest_api('/admin/api/2021-07/products/8084846838064.json', array(), 'GET', $get_details->access_token, $body_data_decode['shopname']);
-
-            $getimfdata = json_decode($getimfsrcdata['body'], true);
-            echo"<pre>getimfdata"; print_r($getimfdata); echo "</pre>";
-            $image_url = null;
-            foreach ($getimfdata['product']['images'] as $image) {
-                // Check if the image is associated with the variant
-                if (in_array('44320678019376', $image['variant_ids'])) {
-                    $image_url = $image['src'];
-                    break;
-                }
-            }
-            echo "image_url=".$image_url;*/
-
+            /** get order details by order number & email**/
 
             $getprietuleid = $this->common->rest_api('/admin/api/2023-07/orders.json?name=' . $body_data_decode['ordernum'] . '&email=' . $body_data_decode['emailf'], array(), 'GET', $get_details->access_token, $body_data_decode['shopname']);
 
@@ -1141,11 +1122,37 @@ class FrontController extends BaseController
             echo "invalid";
         }
     }
-    function get_orders_info(){
+    function get_orders_info()
+    {
         $body_data = file_get_contents('php://input');
         $body_data_decode = json_decode($body_data, TRUE);
-       
+
         $getresultdata = $this->exchange_model->get_order_info($body_data_decode['orderid']);
-        echo"<pre>";print_r($getresultdata);echo"</pre>";
+        echo "<pre>";
+        print_r($getresultdata);
+        echo "</pre>";
+        foreach ($getresultdata as $getorderinfo) {
+            // $getvarimg = $this->common->rest_api('/admin/api/2023-07/variants/44320678019376.json', array(), 'GET', $get_details->access_token, $body_data_decode['shopname']);
+
+            // $getvrntimg = json_decode($getvarimg['body'], true);
+            // //echo"<pre>"; print_r($getvrntimg); echo "</pre>";
+
+
+            // $getimfsrcdata = $this->common->rest_api('/admin/api/2021-07/products/8084846838064.json', array(), 'GET', $get_details->access_token, $body_data_decode['shopname']);
+
+            // $getimfdata = json_decode($getimfsrcdata['body'], true);
+            // echo "<pre>getimfdata";
+            // print_r($getimfdata);
+            // echo "</pre>";
+            // $image_url = null;
+            // foreach ($getimfdata['product']['images'] as $image) {
+            //     // Check if the image is associated with the variant
+            //     if (in_array('44320678019376', $image['variant_ids'])) {
+            //         $image_url = $image['src'];
+            //         break;
+            //     }
+            // }
+            // echo "image_url=" . $image_url;
+        }
     }
 }
