5121f2be78ae5f7d7cc0d722df52a997d8776628
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-26 18:03:37 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -150,6 +150,10 @@ class Home extends BaseController
                     // $getprietuleid = $this->common->rest_api('/admin/api/2023-10/price_rules.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
                     $getprietuleidrec = json_decode($getprietuleid['body'], true);
 
+                    echo "getprietuleid<pre>";
+                    print_r($getprietuleid);
+                    echo "</pre>";
+
                     echo "getprietuleidrec<pre>";
                     print_r($getprietuleidrec);
                     echo "</pre>";
