ce4a9579e97428ed1f63e57fce7f4fc797db1b17
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-27 17:48:04 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -73,6 +73,93 @@ class Home extends BaseController
 
                 if ($_GET['shop'] == 'desinomatetest.myshopify.com') {
 
+                    $getprietuleid = $this->common->rest_api('/admin/api/2023-07/orders/5524761903408.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
+                   
+                    $getprietuleidrec = json_decode($getprietuleid['body'], true);
+
+                    echo "getprietuleidrec<pre>";
+                    print_r($getprietuleidrec);
+                    echo "</pre>";
+
+                    /*$paid_price = 0;
+                    foreach ($jsndata->line_items as $products) {
+                        //if ($products->name != "Partial Pending Payment") {
+                        if ($products->sku == "") {
+
+                            $prosku = 'PRTTESTSKY' . time();
+                        } else {
+                            $prosku = $products->sku;
+                            $prodycprice =  $products->price;
+                        }
+
+                        if (isset($products['properties'][0]['value']) && $products['properties'][0]['value'] == 'Initial Partial Payment') {
+                            $item_price = $products['properties'][2]['value'];
+                            $productvarient = $products['properties'][1]['value'];
+                        } else {
+                            $item_price = $products['price'];
+                            $productvarient = $products['variant_id'];
+                        }
+
+                        $line_item[] = array(
+                            "variant_id" => $productvarient,
+                            "quantity" => $products->quantity,
+                            "gift_card" => true,
+                            "sku" => $prosku,
+                            "grams" => $products->grams,
+                            // "applied_discount" => array(
+                            //     "description" => 'Partial Payment',
+                            //     "title" => 'Partial Payment',
+                            //     "value_type" => "fixed_amount",
+                            //     "value" => $item_price . ".00",
+                            //     "amount" => $item_price . ".00",
+                            // ),
+                            "properties" => array(
+                                array("name" => "Note", "value" => "Actual order"),
+                                array("name" => "full_pay", "value" => $item_price)
+                            ),
+                            "requires_shipping" => true
+                        );
+                        $paid_price = $paid_price + $products['price'];
+
+                        // }
+                    }
+                    $discoutnarray = array(
+                        "code" => "partialcode",
+                        "amount" => $paid_price,
+                        "type" => "fixed_amount",
+                    );
+                    if (isset($jsndata->shipping_address)) {
+
+                        if (isset($jsndata->shipping_address->phone)) {
+                            $store_phnum = str_replace(" ", "", $jsndata->shipping_address->phone);
+                            $store_phnum = str_replace("(", "", $store_phnum);
+                            $store_phnum = str_replace(")", "", $store_phnum);
+                            $store_phnum = str_replace("-", "", $store_phnum);
+                        } else {
+                            $store_phnum = "";
+                        }
+
+                        $actl_shipping_addrss = array(
+                            "first_name" => $jsndata->shipping_address->first_name,
+                            "first_name" => $jsndata->shipping_address->last_name,
+                            "address1" => $jsndata->shipping_address->address1,
+                            "address2" => (isset($jsndata->shipping_address->address2) ? $jsndata->shipping_address->address2 : ''),
+                            "phone" => $store_phnum,
+                            "city" => $jsndata->shipping_address->city,
+                            "province" => $jsndata->shipping_address->province,
+                            "zip" => $jsndata->shipping_address->zip,
+                            "country" => $jsndata->shipping_address->country,
+                        );
+                    } else {
+                        $actl_shipping_addrss = array();
+                    }
+                    $final_array = array("order" => array("line_items" => $line_item, "email" => $jsndata->email, "shipping_address" => $actl_shipping_addrss, "discount_codes" => $discoutnarray));
+
+                    $resposne_array = array("name" => "actual order" . json_encode($final_array));
+                    $this->user_model->check_test_response($resposne_array);
+
+                    $this->common->create_actual_order($get_resulsts->access_token, $_GET['whshp'], $final_array);*/
+
 
                     // $jsondecod = '{"fulfillment":{"line_items_by_fulfillment_order":[{"fulfillment_order_id":1046000814}],"tracking_info":{"number":"MS1562678","url":"https://www.my-shipping-company.com?tracking_number=MS1562678"}}}';
                     // echo "<pre>";
