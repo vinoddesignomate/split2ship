448dbc88322812101c5259758c51ebd72ba99161
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-30 16:11:22 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2257,7 +2257,7 @@ class Home extends BaseController
                 "full_btn_color" => $this->request->getPost('fullbtn_color'),
                 "chk_btn_color" => $this->request->getPost('chk_btn_color'),
                 "full_chk_btn_color" => $this->request->getPost('full_chk_btn_color'),
-                "cart_form_class" => $this->request->getPost('cart_form_class'),
+                //"cart_form_class" => $this->request->getPost('cart_form_class'),
                 "addcartbtn_cg" => $this->request->getPost('addcartbtn_cg'),
                 "cg_chkout_btn_class" => $this->request->getPost('cg_chkout_btn_class'),
                 "cg_cart_remove_class" => $this->request->getPost('cg_cart_remove_class'),
