fb59d989b759fb2af4148ead756e05d63e5d824e
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-10-31 15:36:12 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -81,7 +81,7 @@ class Home extends BaseController
                     //  echo "</pre>";
 
 
-                     $getprietuleid = $this->common->rest_api('/admin/api/2023-07/orders/5528591630640.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
+                     $getprietuleid = $this->common->rest_api('/admin/api/2023-07/orders/5529239355696.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
 
                      $getprietuleidrec = json_decode($getprietuleid['body'], true);
 
