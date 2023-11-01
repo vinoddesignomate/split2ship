07a73b59423c160c7d3b74e622aed26095e1d1dd
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-11-01 19:38:51 +0530

    Update FrontController.php

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -224,7 +224,9 @@ class FrontController extends BaseController
 
                     //if ($_SERVER['HTTP_X_FORWARDED_FOR'] == '103.80.119.106') {
                     if ($item_cart['price'] != $item_cart['item_original_price']) {
-                        $coupencodeprice = $item_cart['item_original_price'] - $item_cart['price'];
+
+                        
+                        $coupencodeprice = $item_cart['item_original_price'] * $item_cart['qty'] - $item_cart['price'];
                         $getcpncodep = $body_data_decode['getcpncode'];
                         if ($getcpncodep != "") {
                             $couponname = $getcpncodep;
