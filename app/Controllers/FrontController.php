9399f5e5ad3f598c0b9b7e16fa53aeed98926442
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-11-02 16:50:23 +0530

    Update FrontController.php

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -317,12 +317,12 @@ class FrontController extends BaseController
             //echo $chekpartial;
             //    print_r($line_item_arra);
 
-            // if ($_SERVER['HTTP_X_FORWARDED_FOR'] == '103.80.119.106') {
-            //     echo "line_item_arra<pre>";
-            //     print_r($line_item_arra);
-            //     echo "</pre>";
+            if ($_SERVER['HTTP_X_FORWARDED_FOR'] == '103.80.119.106') {
+                echo "line_item_arra<pre>";
+                print_r($line_item_arra);
+                echo "</pre>";
 
-            // }
+            }
             $final_total_price_rem = str_replace("-", "", $remaining_price);
 
             $final_array = array("draft_order" => array("line_items" => $line_item_arra, "tags" => "partial_" . $final_total_price_rem));
