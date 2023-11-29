c5b3d5411ea84500b3981e12346ceecc97501684
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-11-29 15:35:56 +0530

    Update FrontController.php

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -929,10 +929,10 @@ class FrontController extends BaseController
                 "full_buy_btn_color" => $gtbtncolor[0]->full_buy_btn_color,
                 "full_buy_text_color" => $gtbtncolor[0]->full_buy_text_color,
                 "partial_dep_text" => ($gtbtncolor[0]->partial_dep_text !="") ? $gtbtncolor[0]->partial_dep_text : 'Partial Deposit',
-                "remaining_txtcrt" => isset($gtbtncolor[0]->remaining_txtcrt) ? $gtbtncolor[0]->remaining_txtcrt : 'Remaining Balance',
-                "cart_three_txt" => isset($gtbtncolor[0]->cart_three_txt) ? $gtbtncolor[0]->cart_three_txt : 'You need to pay remaining balance to delivery person',
-                "cg_partial_btn_text" => isset($gtbtncolor[0]->cg_partial_btn_text) ? $gtbtncolor[0]->cg_partial_btn_text : 'Partial Payment',
-                "cg_full_btn_text" => isset($gtbtncolor[0]->cg_full_btn_text) ? $gtbtncolor[0]->cg_full_btn_text : 'Full Payment',
+                "remaining_txtcrt" => ($gtbtncolor[0]->remaining_txtcrt !="") ? $gtbtncolor[0]->remaining_txtcrt : 'Remaining Balance',
+                "cart_three_txt" => ($gtbtncolor[0]->cart_three_txt !="") ? $gtbtncolor[0]->cart_three_txt : 'You need to pay remaining balance to delivery person',
+                "cg_partial_btn_text" => ($gtbtncolor[0]->cg_partial_btn_text !="") ? $gtbtncolor[0]->cg_partial_btn_text : 'Partial Payment',
+                "cg_full_btn_text" => ($gtbtncolor[0]->cg_full_btn_text !="") ? $gtbtncolor[0]->cg_full_btn_text : 'Full Payment',
 
             );
             return json_encode($return_array);
