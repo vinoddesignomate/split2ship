58b347f12a4185a745f8917766e6369fa94d1ec6
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-12-05 20:15:30 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -102,17 +102,17 @@ class Home extends BaseController
                     //  echo "</pre>";
 
 
-                    // $getprietuleid = $this->common->rest_api('/admin/api/2023-07/orders/5402588840149.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
+                    $getprietuleid = $this->common->rest_api('/admin/api/2023-07/orders/5577527591216.json', array(), 'GET', $get_details->access_token, $_GET['shop']);
 
-                    // $getprietuleidrec = json_decode($getprietuleid['body'], true);
+                    $getprietuleidrec = json_decode($getprietuleid['body'], true);
 
-                    // // $etisus = '[{"code":"FAKE30","amount":"9.00","type":"percentage"}]';
+                    // $etisus = '[{"code":"FAKE30","amount":"9.00","type":"percentage"}]';
 
 
-                    // echo "getprietuleidrec<pre>";
-                    // print_r($getprietuleidrec);
-                    // echo "</pre>";
-                    // die();
+                    echo "getprietuleidrec<pre>";
+                    print_r($getprietuleidrec);
+                    echo "</pre>";
+                    //die();
                     /*
                     $paid_price = 0;
                     $total_item_price = 0;
