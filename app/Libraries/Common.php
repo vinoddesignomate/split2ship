f932cbe007dd2110a934bbc304d59863b104cbbd
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2023-12-07 20:16:08 +0530

    Update Common.php

--- app/Libraries/Common.php
+++ app/Libraries/Common.php
@@ -54,7 +54,9 @@ class Common
             curl_setopt($curl, CURLOPT_POSTFIELDS, $query);
         }
         $response = curl_exec($curl);
-        // echo "response=".$response;
+        if($shop_url == 'desinomatetest.myshopify.com'){
+        echo "response=".$response;
+        }
         $error = curl_errno($curl);
         $error_msg  = curl_error($curl);
         curl_close($curl);
