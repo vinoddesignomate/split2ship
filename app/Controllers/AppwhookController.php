8ba4d4f13310c485999df3aaddc47f8318674434
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2024-01-01 21:18:48 +0530

    Update AppwhookController.php

--- app/Controllers/AppwhookController.php
+++ app/Controllers/AppwhookController.php
@@ -419,12 +419,14 @@ class AppwhookController extends BaseController
 
         $get_actual_orders = $this->common->create_actual_order($get_resulsts->access_token, $_GET['whshp'], $order_data);
 
-        // $resposne_array = array("name" => "plus order" . $_GET['whshp']);
-        // $this->user_model->check_test_response($resposne_array); 
+
 
         $this->user_model->update_plan_orders_plus(1, $_GET['whshp']); //plus 1 order
 
         $decode_get_actual_orders = json_decode($get_actual_orders);
+
+        $resposne_arrayjjk = array("name" => "get_actual_orders" . $get_actual_orders);
+        $this->user_model->check_test_response($resposne_arrayjjk);
         if (isset($decode_get_actual_orders->order->id)) {
 
             //update second order which is created by API
