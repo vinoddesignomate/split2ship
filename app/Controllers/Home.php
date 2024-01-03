634a4da7860a32173cb040008475a06388b1d97d
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2024-01-03 16:35:31 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -962,14 +962,14 @@ class Home extends BaseController
                         $this->user_model->update_plan_products($total_synproduct, $_GET['shop']);
                         echo "<script>top.window.location='https://admin.shopify.com/store/" . $this->shope_name . "/apps/pay-x-now-rest-on-delivery/partial-latest-products-list'</script>";
                     } else {
-                        echo "<script>alert('Please upgrade the plan'); top.window.location='https://admin.shopify.com/store/" . $this->shope_name . "/apps/pay-x-now-rest-on-delivery/price-plan'</script>";
+                        echo "<script>alert('Please upgrade the plan1'); top.window.location='https://admin.shopify.com/store/" . $this->shope_name . "/apps/pay-x-now-rest-on-delivery/price-plan'</script>";
 
                         // $data['pricurl'] = "https://admin.shopify.com/store/" . $this->shope_name . "/apps/pay-x-now-rest-on-delivery/price-plan";
                         // echo view('templates/apbrdgnew', $data);
 
                     }
                 } else {
-                    echo "<script>alert('Please upgrade the plan'); top.window.location='https://admin.shopify.com/store/" . $this->shope_name . "/apps/pay-x-now-rest-on-delivery/price-plan'</script>";
+                    echo "<script>alert('Please upgrade the plan2'); top.window.location='https://admin.shopify.com/store/" . $this->shope_name . "/apps/pay-x-now-rest-on-delivery/price-plan'</script>";
 
                     // $data['pricurl'] = "https://admin.shopify.com/store/" . $this->shope_name . "/apps/pay-x-now-rest-on-delivery/price-plan";
                     // echo view('templates/apbrdgnew', $data);
