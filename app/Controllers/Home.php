d0ac69fb82d2c322499322f2116b73f5d4192e3e
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2024-01-10 17:55:44 +0530

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -537,11 +537,11 @@ class Home extends BaseController
                     print_r($order_data);
                     echo "</pre>";
 
-                    // $getorderarry = $this->common->create_actual_order($get_details->access_token, $_GET['shop'], $order_data);
+                    $getorderarry = $this->common->create_actual_order($get_details->access_token, $_GET['shop'], $order_data);
 
-                    // echo "<pre>";
-                    // print_r(json_decode($getorderarry));
-                    // echo "</pre>";
+                    echo "<pre>";
+                    print_r(json_decode($getorderarry));
+                    echo "</pre>";
 
                     // echo $getorderarry;
                     // }
