6c8915704cc0839e65eb4d1a9ac21c666e622444
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2024-01-16 22:12:03 +0530

    Update install_config_stp.php

--- app/Views/install_config_stp.php
+++ app/Views/install_config_stp.php
@@ -1,23 +1,14 @@
 <style>
-    .popupCgTransparancy {
-        position: relative;
-    }
-
-    .popupCgTransparancy:before {
+    #showPopUpCG {
+        display: none;
         width: 100%;
         background: rgba(0, 0, 0, 0.5);
         float: left;
-        position: absolute;
+        position: fixed;
         top: 0;
         left: 0;
         width: 100%;
-        height: 100%;
-        z-index: 9999;
-    }
-
-    #showPopUpCG {
-        display: none;
-        position: relative;
+        min-height: 100%;
         z-index: 99999;
     }
 
@@ -265,8 +256,6 @@
         gap: 15px;
     }
 
-    .inlineButtonsCg ul li {}
-
     .inlineButtonsCg ul li a {
         min-width: 120px;
         max-width: 200px;
@@ -313,8 +302,8 @@
         width: 0 !important;
         transition: all .1s ease-in-out !important;
         visibility: hidden;
-        display: none;
     }
+
 
     @media screen and (max-width:479px) {
         .inlineButtonsCgTabs ul li {
@@ -462,7 +451,7 @@
 <script>
     $("#showPopUpCG").fadeIn();
     $('body').addClass('popupCgTransparancy');
-
+    
     /*$("#popupCGclick").click(function() {
         $("#showPopUpCG").fadeIn();
     });*/
