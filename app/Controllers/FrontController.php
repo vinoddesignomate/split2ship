496ae52caf0d3dacdb333d1c34bc421caa0d5b84
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2024-01-22 15:45:17 +0530

    Update FrontController.php

--- app/Controllers/FrontController.php
+++ app/Controllers/FrontController.php
@@ -294,9 +294,9 @@ class FrontController extends BaseController
             $getprietuleid = $this->common->rest_api('/admin/api/2023-10/price_rules.json', $creatruledata, 'POST', $get_details->access_token, $shopname);
 
             $getprietuleidrec = json_decode($getprietuleid['body'], true);
-            if ($shopname == 'tajbridalindia.myshopify.com') {
-                print_r($getprietuleidrec);
-            }
+            // if ($shopname == 'tajbridalindia.myshopify.com') {
+            //     print_r($getprietuleidrec);
+            // }
             if (array_key_exists('errors', $getprietuleidrec)) {
                 echo "invalid";
             } else {
@@ -310,10 +310,10 @@ class FrontController extends BaseController
 
                 $createcoupon = $this->common->rest_api('/admin/api/2023-10/price_rules/' . $getprietuleidrec['price_rule']['id'] . '/discount_codes.json', $creatediscode, 'POST', $get_details->access_token, $shopname);
                 $createcouponrec = json_decode($createcoupon['body'], true);
-                if ($shopname == 'tajbridalindia.myshopify.com') {
-                    echo "createcouponrec";
-                    print_r($createcouponrec);
-                }
+                // if ($shopname == 'tajbridalindia.myshopify.com') {
+                //     echo "createcouponrec";
+                //     print_r($createcouponrec);
+                // }
                 if (array_key_exists('errors', $createcouponrec)) {
                     echo "invalid";
                 } else {
