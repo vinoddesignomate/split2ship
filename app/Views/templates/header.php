18cb6c0958c2941d5cb161fb9237acfde8c05f5b
vinoddesignomate <92621297+vinoddesignomate@users.noreply.github.com>
2024-02-13 16:04:28 +0530

    Update header.php

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -90,7 +90,7 @@ if ($page_name == "index.php" || $page_name == "mainpage") {
 
     <!-- Header section -->
     <header>
-        <div class="relativeLoaderCG56">
+        <div class="relativeLoaderCG56" style="display: none;">
             <div class="loaderCgApp4"></div>
         </div>
         <div class="payxnowandrestondelivery-container">
